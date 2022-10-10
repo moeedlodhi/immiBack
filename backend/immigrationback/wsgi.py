6f04da616b064a5a1c11ed18770752b48c6ab93b
moeed.lodhi <moeedlodhi@hgmail.com>
2022-10-10 23:52:03 +1100

    new

--- backend/immigrationback/wsgi.py
+++ backend/immigrationback/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'immigrationback.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'immigrationback.settings.test')
 
 application = get_wsgi_application()
