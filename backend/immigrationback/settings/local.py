2e9c370b7352c57ebac3c82c7594a9236d55cd4a
moeed.lodhi <moeedlodhi@hgmail.com>
2022-11-26 02:11:50 +1100

    added

--- backend/immigrationback/settings/local.py
+++ backend/immigrationback/settings/local.py
@@ -178,6 +178,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = [
+    'localhost:4200'
+]
 
 AUTH_USER_MODEL = 'authentication.User'
